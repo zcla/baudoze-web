62205ee38e26a47fffd3ccd1f83fb93452fd58a7
ze <zeclaudioaguiar@gmail.com>
2025-02-13 22:35:19 +0000

    O começo de tudo!

---
+++ src/test/java/ze/bau/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ze.bau.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
